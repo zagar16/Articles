a50ca52d5220932a47222592db84817db6743a70
zagar16 <zagar.domen1@gmail.com>
2023-11-21 20:25:30 +0100

    added add_articles and get_articles

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/articlesflask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db= SQLAlchemy(app)
+db = SQLAlchemy(app)
+ma = Marshmallow(app) 
 
 
 class Articles(db.Model):
@@ -20,10 +22,43 @@ class Articles(db.Model):
         self.title = title
         self.body = body
 
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id','title','body','date')
 
-@app.route('/', methods= ['GET'])
+article_shema = ArticleSchema()
+articles_shema = ArticleSchema(many=True)
+
+
+
+@app.route('/get', methods= ['GET'])
 def get_articles():
-    return jsonify({"Hello":"World"})
+    #return jsonify({"Hello":"World"})
+    all_articles = Articles.query.all()
+    results = articles_shema.dump(all_articles)
+    return jsonify(results)
+
+
+@app.route('/get/<id>/', methods= ['GET'])
+def post_details(id):
+    article = Articles.query.get(id)
+    return articles_shema.jsonify(article)
+     
+
+
+
+@app.route('/add', methods= ['POST'])
+def add_article():
+    title = request.json['title']
+    body = request.json['body']
+
+    articles = Articles(title, body)
+    db.session.add(articles)
+    db.session.commit()
+
+    return article_shema.jsonify(articles)
+
+
 
 # Add the following block to create tables within the Flask application context
 with app.app_context():
